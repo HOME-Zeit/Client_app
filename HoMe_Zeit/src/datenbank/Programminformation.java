0cb26e68fdfe52679af938fd18ac948fd4b1af62
ViktorOsa <viktor.osadchyi1@gmail.com>
2014-11-25 09:58:45 +0100

    nichts neues

--- HoMe_Zeit/src/datenbank/Programminformation.java
+++ HoMe_Zeit/src/datenbank/Programminformation.java
@@ -8,8 +8,8 @@ public class Programminformation
 	public Long endzeit;
 	public String prod_verantwortlicher;
 	public String sende_verantwortlicher;
-	public Long reale_startzeit; // 
-	public Long reale_endzeit;
+	public Long reale_startzeit; // null
+	public Long reale_endzeit; // null
 	
 		
 	public Programminformation(Integer nummer, String titel, Long startzeit, Long endzeit, String prod_verantwortlicher, String sende_verantwortlicher, Long reale_startzeit, Long reale_endzeit)
