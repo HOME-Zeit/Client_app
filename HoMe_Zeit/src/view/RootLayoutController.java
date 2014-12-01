d09b2baff4c758315278f918f2096098a2cfe440
ViktorOsa <viktor.osadchyi1@gmail.com>
2014-12-01 23:08:00 +0100

    Have commented not-used things:

--- HoMe_Zeit/src/view/RootLayoutController.java
+++ HoMe_Zeit/src/view/RootLayoutController.java
@@ -47,68 +47,68 @@ public class RootLayoutController {
 	 */
 	@FXML
 	private void handleNew() {
-		main.getProgrammData().clear();
-		main.setProgrammFilePath(null);
+//		main.getProgrammData().clear();
+//		main.setProgrammFilePath(null);
 	}
-
+	
 	/**
 	 * Opens a FileChooser to let the user select program book to load.
 	 */
 	@FXML
 	private void handleOpen() {
-		FileChooser fileChooser = new FileChooser();
-
-		// Set extension filter
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
-				"XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-
-		// Show save file dialog
-		File file = fileChooser.showOpenDialog(main.getPrimaryStage());
-
-		if (file != null) {
-			main.loadProgrammDataFromFile(file);
-		}
+//		FileChooser fileChooser = new FileChooser();
+//
+//		// Set extension filter
+//		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+//				"XML files (*.xml)", "*.xml");
+//		fileChooser.getExtensionFilters().add(extFilter);
+//
+//		// Show save file dialog
+//		File file = fileChooser.showOpenDialog(main.getPrimaryStage());
+//
+//		if (file != null) {
+//			main.loadProgrammDataFromFile(file);
+//		}
 	}
-
+	
 	/**
 	 * Saves the file to the programm file that is currently open. If there is
 	 * no open file, the "save as" dialog is shown.
 	 */
 	@FXML
 	private void handleSave() {
-		File personFile = main.getProgrammFilePath();
-		if (personFile != null) {
-			main.saveProgrammDataToFile(personFile);
-		} else {
-			handleSaveAs();
-		}
+//		File personFile = main.getProgrammFilePath();
+//		if (personFile != null) {
+//			main.saveProgrammDataToFile(personFile);
+//		} else {
+//			handleSaveAs();
+//		}
 	}
-
+	
 	/**
 	 * Opens a FileChooser to let the user select a file to save to.
 	 */
 	@FXML
 	private void handleSaveAs() {
-		FileChooser fileChooser = new FileChooser();
-
-		// Set extension filter
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
-				"XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-
-		// Show save file dialog
-		File file = fileChooser.showSaveDialog(main.getPrimaryStage());
-
-		if (file != null) {
-			// Make sure it has the correct extension
-			if (!file.getPath().endsWith(".xml")) {
-				file = new File(file.getPath() + ".xml");
-			}
-			main.saveProgrammDataToFile(file);
-		}
+//		FileChooser fileChooser = new FileChooser();
+//
+//		// Set extension filter
+//		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+//				"XML files (*.xml)", "*.xml");
+//		fileChooser.getExtensionFilters().add(extFilter);
+//
+//		// Show save file dialog
+//		File file = fileChooser.showSaveDialog(main.getPrimaryStage());
+//
+//		if (file != null) {
+//			// Make sure it has the correct extension
+//			if (!file.getPath().endsWith(".xml")) {
+//				file = new File(file.getPath() + ".xml");
+//			}
+//			main.saveProgrammDataToFile(file);
+//		}
 	}
-
+	
 	/**
 	 * Opens an about dialog.
 	 */
