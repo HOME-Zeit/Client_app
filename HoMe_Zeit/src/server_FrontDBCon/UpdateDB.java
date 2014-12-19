6e373861e22b1537c1d9cde27728b18e228e84bf
Bollwerker <Bollwerker@users.noreply.github.com>
2014-12-19 01:24:07 +0100

    Abschnitt(Segment) Bearbeitungsmethoden gesetzt
    segmentsRegister(Programminformation p, boolean truefalse)
    //trägt neuen Abschnitt ein mittels der gesamzen Programminformation
    segmentsRegister(ArrayList<Abschnitt> segment, boolean truefalse)
    //trägt neue Programmabschnitte ein mit der gesamten ArrayList
    segmentsRegister(Abschnitt segment, boolean truefalse)
    //trägt neuen Abschnitt ein mit genau dem beschriebenen Abschnitt
    
    segmentsDelete(int number, boolean truefalse)
    //löscht mit Nummer gewählten Abschnitt
    segmentsRefactor(Abschnitt segment, boolean truefalse)
    //bearbeitet gewählten abschnitt indem dieser überschrieben wir

--- HoMe_Zeit/src/server_FrontDBCon/UpdateDB.java
+++ HoMe_Zeit/src/server_FrontDBCon/UpdateDB.java
@@ -1,6 +1,7 @@
 package server_FrontDBCon;
 
 import datenbank.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -22,5 +23,36 @@ public class UpdateDB
     {
         //TODO refactor selected file
         Datenbank.bearbeiten(p,truefalse);
+    }
+    
+    //-----
+    
+    public static void segmentsRegister(Programminformation p, boolean truefalse)
+    {
+        //TODO Register a new Segment with the whole Programminformation
+        Datenbank.abschnitte_eintragen(p,truefalse);
+    }
+    public static void segmentsRegister(ArrayList<Abschnitt> segment, boolean truefalse)
+    {
+        //TODO Register a new Segment with an ArrayList of Segments
+        Datenbank.abschnitte_eintragen(segment,truefalse);
+    }
+    public static void segmentsRegister(Abschnitt segment, boolean truefalse)
+    {
+        //TODO Register a new Segment with this single Segment
+        Datenbank.abschnitte_eintragen(segment,truefalse);
+    }
+    
+    //-----
+    
+    public static void segmentsDelete(int number, boolean truefalse)
+    {
+        //TODO Delete a Segment with its number
+        Datenbank.abschnitt_loeschen(number,truefalse);
+    }
+    public static void segmentsRefactor(Abschnitt segment, boolean truefalse)
+    {
+        //TODO Refactor a Segment by overwriting it directly
+        Datenbank.abschnitt_bearbeiten(segment,truefalse);
     }
 }
